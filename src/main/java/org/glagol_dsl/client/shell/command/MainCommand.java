b3ebe43384aa8864f927bde42ffccc2ed48c027d
Yoan-Alexander Grigorov <joan.grigorov@gmail.com>
2018-05-19 23:25:25 +0200

    Added SSH authentication and encrypted communication through local tunnelling

--- src/main/java/org/glagol_dsl/client/shell/command/MainCommand.java
+++ src/main/java/org/glagol_dsl/client/shell/command/MainCommand.java
@@ -1,5 +1,12 @@
 package org.glagol_dsl.client.shell.command;
 
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import com.jcraft.jsch.agentproxy.AgentProxyException;
+import com.jcraft.jsch.agentproxy.Connector;
+import com.jcraft.jsch.agentproxy.ConnectorFactory;
+import com.jcraft.jsch.agentproxy.RemoteIdentityRepository;
 import org.glagol_dsl.client.ConsoleStream;
 import org.glagol_dsl.client.Version;
 import org.glagol_dsl.client.socket.Client;
@@ -8,14 +15,16 @@ import picocli.CommandLine.Option;
 import java.io.IOException;
 
 @picocli.CommandLine.Command(
-    name = "Glagol DSL Client",
+    name = "glagol",
     version = {Version.VERSION}
 )
 public class MainCommand implements Command {
 
-    private static final String DEFAULT_HOST = "127.0.0.1";
-
+    private static final String DEFAULT_HOST = "35.204.215.192";
     private static final Integer DEFAULT_PORT = 51151;
+
+    private static final String TUNNEL_REMOTE_HOST = "glagol-server-svc.default.svc.cluster.local";
+    private static final int TUNNEL_REMOTE_PORT = 51151;
 
     @Option(names = {"-h", "--help"}, usageHelp = true, description = "Display this help message")
     private boolean usageHelpRequested;
@@ -28,6 +37,11 @@ public class MainCommand implements Command {
 
     @Option(names = {"-p", "--port"}, description = "Host port on which the Glagol DSL Server is running")
     private Integer port;
+
+    @Option(names = {"-l", "--no-ssh-auth"}, description = "Suspends ssh authentication. Note: glagol.io requires ssh authentication")
+    private boolean suspendAuth;
+
+    private Session session;
 
     public MainCommand() {
         host = lookupHost(DEFAULT_HOST);
@@ -45,7 +59,7 @@ public class MainCommand implements Command {
     }
 
     Client createClient() {
-        return new Client(host, port);
+        return new Client(suspendAuth ? lookupHost("localhost") : "localhost", port);
     }
 
     private boolean isEmpty(String host) {
@@ -59,8 +73,11 @@ public class MainCommand implements Command {
      */
     @Override
     public void execute(CompileCommand command, ConsoleStream consoleStream) throws IOException {
+        openTunnel();
         command.execute(this, consoleStream);
+        closeTunnel();
     }
+
     /**
      * This method is only used for double-dispatching
      *
@@ -69,5 +86,49 @@ public class MainCommand implements Command {
     @Override
     public void execute(CleanCommand command, ConsoleStream consoleStream) throws IOException {
         command.execute(this, consoleStream);
+    }
+
+    private void openTunnel() {
+        if (suspendAuth) {
+            System.out.println(
+                    "Warning: ssh authentication is disabled. " +
+                    "This can be potentially dangerous since the communication with the server will not be encrypted.");
+            return;
+        }
+
+        JSch.setConfig("StrictHostKeyChecking", "no");
+
+        JSch jSch = new JSch();
+
+        try {
+            ConnectorFactory cf = ConnectorFactory.getDefault();
+            Connector con = cf.createConnector();
+            jSch.setIdentityRepository(new RemoteIdentityRepository(con));
+        } catch (AgentProxyException e) {
+            e.printStackTrace(System.err);
+            return;
+        }
+
+        try {
+            session = jSch.getSession("glagol-client", host);
+            session.connect();
+            session.setPortForwardingL(port, TUNNEL_REMOTE_HOST, TUNNEL_REMOTE_PORT);
+        } catch (JSchException e) {
+            System.err.println("Error: unable to authenticate through SSH: " + e.getMessage());
+            System.exit(255);
+        }
+    }
+
+    private void closeTunnel() {
+        if (session == null) {
+            return;
+        }
+
+        try {
+            session.delPortForwardingL(port);
+            session.disconnect();
+        } catch (JSchException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/org/glagol_dsl/client/socket/Client.java
+++ src/main/java/org/glagol_dsl/client/socket/Client.java
@@ -14,6 +14,7 @@ import java.net.Socket;
 import static java.util.Base64.getDecoder;
 
 public class Client {
+
     private final String host;
     private final Integer port;
 
@@ -25,6 +26,7 @@ public class Client {
     }
 
     public void makeRequest(Request request, Handler responseHandler) throws IOException {
+
         Socket socket = createSocket();
 
         request.sendTo(new PrintWriter(socket.getOutputStream(), true));
